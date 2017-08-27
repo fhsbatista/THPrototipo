04155c7136aea6c3a12a291cb09fff28b1f84b9c
Fernando Batista <fernando.batista@outlook.com>
2017-08-27 09:38:25 -0300

    Login e cadastro de usuario funcionando. Aqui, também já está funcionando a inserção de dados do usuário no banco firebase

--- app/src/main/java/br/com/talkhub/thprototipo/Classes/Usuario.java
+++ app/src/main/java/br/com/talkhub/thprototipo/Classes/Usuario.java
@@ -9,9 +9,27 @@ public class Usuario {
     private String nome;
     private String sobrenome;
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public Usuario(){
+        //Construtor criado apenas para receber um objeto que poderá ser usado no objeto do Firebase
+    }
+
     public Usuario(String email, String nome, String sobrenome) {
         this.email = email;
         this.nome = nome;
         this.sobrenome = sobrenome;
     }
+
+
 }
--- app/src/main/java/br/com/talkhub/thprototipo/MainActivity.java
+++ app/src/main/java/br/com/talkhub/thprototipo/MainActivity.java
@@ -1,16 +1,125 @@
 package br.com.talkhub.thprototipo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import br.com.talkhub.thprototipo.Classes.Usuario;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private EditText mEmail;
+    private EditText mSenha;
+    private Button mEntrar;
+    private Button mCadastrar;
+
+    private DatabaseReference mRef;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mRef = FirebaseDatabase.getInstance().getReference();
+        mEmail = (EditText) findViewById(R.id.et_email_login);
+        mSenha = (EditText) findViewById(R.id.et_senha_login);
+        mEntrar = (Button) findViewById(R.id.bt_entrar_login);
+        mCadastrar = (Button) findViewById(R.id.bt_cadastrar_login);
+
+        //Evento de clique no botão "Logar"
+        mEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String senha = mSenha.getText().toString();
+
+                logar(email, senha);
+
+            }
+        });
+        //Evento de clique no botão "Cadastrar"
+        mCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String senha = mSenha.getText().toString();
+                cadastrar(email, senha);
+            }
+        });
+        //Listener que verifica status da autenticação
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //Se o usuário estiver logado, é executada a ação dentro deste if
+                if(firebaseAuth.getCurrentUser() != null){
+                    Toast.makeText(MainActivity.this, "Logado com sucesso", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
     }
 
-     
+    //Este método faz a validação dos campos e tenta fazer o login no firebase
+    public void logar(String email, String senha){
+        //Se algum campo do login tiver ficado em branco, será dada a mensagem abaixo.
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)){
+            Toast.makeText(MainActivity.this, "Campos em branco", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            mAuth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Algo deu errado", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+    //Este método faz o cadastro de um novo usuário, e já salva dados no database também
+    public void cadastrar(String email, String senha){
 
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(senha)){
+            Toast.makeText(MainActivity.this, "Campos em branco", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Usuario usuario =  new Usuario(email, "", "");
+            mRef.child("usuarios").push().setValue(usuario);
+
+
+            mAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Algo deu errado", Toast.LENGTH_SHORT);
+                    }
+                }
+            });
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 }
